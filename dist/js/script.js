68214bc19b62ff1d01ed52d332b5da6134ff5025
abinesh8900 <abineshranganathan@gmail.com>
2021-08-08 23:35:14 +0530

    rwd-md

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,1 +1,1 @@
-const topHeader=document.getElementById("main-header");console.log(topHeader),window.addEventListener("scroll",(function(){topHeader.classList.toggle("sticky-nav",window.scrollY>0)}));
+const topHeader=document.getElementById("main-header");window.addEventListener("scroll",(function(){topHeader.classList.toggle("sticky-nav",window.scrollY>0)}));
--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,4 @@
 const topHeader = document.getElementById("main-header");
-console.log(topHeader);
 
 window.addEventListener("scroll", function () {
   topHeader.classList.toggle("sticky-nav", window.scrollY > 0);
