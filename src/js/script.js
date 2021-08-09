fe524fa4ee5ad5c6dd781ec3b3e7d97827d4611a
abinesh8900 <abineshranganathan@gmail.com>
2021-08-09 14:29:26 +0530

    rwd mobile

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,14 @@
 const topHeader = document.getElementById("main-header");
+const navMenu = document.getElementById("menu-btn");
+const showNav = document.getElementById("show-nav");
+const mainBody = document.getElementById("main");
 
 window.addEventListener("scroll", function () {
   topHeader.classList.toggle("sticky-nav", window.scrollY > 0);
+});
+
+navMenu.addEventListener("click", function () {
+  navMenu.classList.toggle("active");
+  showNav.classList.toggle("hidden");
+  mainBody.classList.toggle("menu-is-open");
 });
