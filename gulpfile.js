2febd1570363d7afcc238a1391d208163bc38ed5
abinesh8900 <abineshranganathan@gmail.com>
2021-08-10 22:48:52 +0530

    logo as png

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,10 @@
 //list dependences
 const { src, dest, watch, series } = require("gulp");
 const sass = require("gulp-sass")(require("node-sass"));
-const prefix = require("gulp-autoprefixer");
 const minify = require("gulp-clean-css");
 const terser = require("gulp-terser");
-const imagemin = require("gulp-imagemin");
-const imagewebp = require("gulp-webp");
+// const imagewebp = require("gulp-webp");
+// const imagemin = require("gulp-imagemin");
 const browserSync = require("browser-sync").create();
 
 //crearte functions
@@ -14,7 +13,6 @@ const browserSync = require("browser-sync").create();
 function compilescss() {
   return src("src/scss/*.scss")
     .pipe(sass())
-    .pipe(prefix())
     .pipe(minify())
     .pipe(dest("dist/css"));
 }
@@ -24,20 +22,24 @@ function jsmin() {
   return src("src/js/*.js").pipe(terser()).pipe(dest("dist/js"));
 }
 
-//compress images
+// //images
+// function optimizeimg() {
+//   return src("src/images/*.{jpg,png}")
+//     .pipe(
+//       imagemin([
+//         imagemin.mozjpeg({ quality: 80, progressive: true }),
+//         imagemin.optipng({ optimizationLevel: 2 }),
+//       ])
+//     )
+//     .pipe(dest("dist/images"));
+// }
 
-function compressImg() {
-  return src("src/images/*{jpg,png}")
-    .pipe(imagemin())
-    .pipe(dest("dist/images"));
-}
-
-//webp images
-function webpImage() {
-  return src("dist/images/*{jpg,png}")
-    .pipe(imagewebp())
-    .pipe(dest("dist/images"));
-}
+// //webp images
+// function webpImage() {
+//   return src("dist/images/*{jpg,png}")
+//     .pipe(imagewebp())
+//     .pipe(dest("dist/images"));
+// }
 
 //browserSync
 function browserSyncServer(cb) {
@@ -59,16 +61,16 @@ function watchTask() {
   watch("*.html", browserSyncRelode);
   watch("src/scss/**/*.scss", series(compilescss, browserSyncRelode));
   watch("src/js/**/*.js", series(jsmin, browserSyncRelode));
-  watch("src/js/**/*.jpg,png}", compressImg);
-  watch("dist/images/*.{jpg,png}", webpImage);
+  // watch("src/images/*.{jpg,png}", browserSyncRelode);
+  // watch("dist/images/*.{jpg,png}", webpImage);
 }
 // default gulp
 
 exports.default = series(
   compilescss,
   jsmin,
-  compressImg,
-  webpImage,
+  // optimizeimg,
+  // webpImage,
   browserSyncServer,
   watchTask
 );
